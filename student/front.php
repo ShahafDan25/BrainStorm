a107b4953204460d7b42648c5ccf0a6c24f62f8a
Shahaf Dan <dan.shachaf@gmail.com>
2020-06-03 03:55:58 -0700

    minor changes

--- student/front.php
+++ student/front.php
@@ -1,20 +1,27 @@
 <?php  include "../general/funcs.php" ?>
 <!DOCTYPE html>
 <html>
-  <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
-      <title>BrainStorm - Student</title>
-      <link rel="stylesheet" href="studLoginStyle.css" />
-  </head>
-  <body>
-    <div class="BG"></div>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+        <title>BrainStorm - Student</title>
+        <link rel="stylesheet" href="studLoginStyle.css" />
+    </head>
+    <body>
+
+        <div class="BG"></div>
+        <!-- <header>
+                <img class="logo" src="../indexes/img/BsStudent.png" alt="logo" href="../indexes/studentIndex.html"/>
+                <button class = "navBtn inline" onclick = "location.replace('../student/mypath.php');" >My Path</button>
+                <button class = "navBtn inline" onclick = "location.replace('../student/profile.php');" >Profile</button>
+        </header> -->
         <div class="container" id="container">
-        <div class="form-container sign-up-container">
+        
+            <div class="form-container sign-up-container">
             <!-- EDIT FORM ACTION FOR LINK -->
-            <form action="#">
+            <form action="../general/funcs.php" method = "POST">
                 <h1>Create Account</h1>
                 <div class="social-container">
                     <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
@@ -34,37 +41,38 @@
                 <button>Sign Up</button>
             </form>
         </div>
-    <div class="form-container sign-in-container">
-        <form action = "../general/funcs.php" method = "POST">
-            <h1>Log In</h1>
-            <div class="social-container">
-                <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-            </div>
-            <span>or use your account</span>
-            <input type="email" placeholder="Email" name = "email"/>
-            <input type="password" placeholder="Password" name = "password"/>
-            <input type = "hidden" name = "message" value = "loginstudent">
-            <a href="#">Forgot your password?</a>
-            <button>Log In</button>
-        </form>
-    </div>
-    <div class="overlay-container">
-        <div class="overlay">
-            <div class="overlay-panel overlay-left">
-                <h1>Welcome Back!</h1>
-                <p>Login with your personal info to connect with us again</p>
-                <button class="ghost" id="signIn">Log In</button>
-            </div>
-            <div class="overlay-panel overlay-right">
-                <h1>Hello There!</h1>
-                <p>Start your journey with us by creating a new account</p>
-                <button class="ghost" id="signUp">Sign Up</button>
+        <div class="form-container sign-in-container">
+            <form action = "../general/funcs.php" method = "POST">
+                <h1>Log In</h1>
+                <div class="social-container">
+                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your account</span>
+                <input type="email" placeholder="Email" name = "email"/>
+                <input type="password" placeholder="Password" name = "password"/>
+                <input type = "hidden" name = "message" value = "loginstudent">
+                <a href="#">Forgot your password?</a>
+                <button>Log In</button>
+            </form>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                    <h1>Welcome Back!</h1>
+                    <p>Login with your personal info to connect with us again</p>
+                    <button class="ghost" id="signIn">Log In</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <h1>Hello There!</h1>
+                    <p>Start your journey with us by creating a new account</p>
+                    <button class="ghost" id="signUp">Sign Up</button>
+                </div>
             </div>
         </div>
-    </div>
-    </div>
+        
+    </body>
     <script>
         const signUpButton = document.getElementById('signUp');
         const signInButton = document.getElementById('signIn');
@@ -118,6 +126,4 @@
             //send email
         });
     </script>
-  </body>
-  
 </html>
